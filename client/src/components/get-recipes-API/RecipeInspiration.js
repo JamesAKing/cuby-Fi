1d3dfdbf27eaafbe75b175eac41808afd922cf50
James King <james_andrew_king@hotmail.co.uk>
2021-04-04 12:52:31 -0700

    connected RecipeInspiration so that it now adds a new meal to the RecipesDB

--- client/src/components/get-recipes-API/RecipeInspiration.js
+++ client/src/components/get-recipes-API/RecipeInspiration.js
@@ -2,6 +2,7 @@ import "./RecipeInspiration.scss";
 import { useState } from 'react';
 import axios from 'axios';
 import { createRecipeObj } from '../../utilities/functions';
+import { RecipesDB_URL } from '../../utilities/APIEndPoints';
 import RecipeCard from '../recipe-card/RecipeCard'; 
 
 const theMealDB_URL = 'https://www.themealdb.com/api/json/v1/1';
@@ -11,7 +12,6 @@ function RecipeInspiration() {
     const [ recipeIdea, setRecipeIdea ] = useState(null);
 
     const getRandomMeal = () => {
-
         axios
             .get(`${theMealDB_URL}/random.php`)
             .then(resp=> {
@@ -21,12 +21,20 @@ function RecipeInspiration() {
             .catch(err => console.log(err))
     }
 
+    const addNewMealToDB = () => {
+        axios
+            .post(RecipesDB_URL, recipeIdea)
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err));
+    }
+
     console.log(recipeIdea);
 
     return (
         <div>
             <button type="button" onClick={getRandomMeal}>Get Random Meal</button>
             {recipeIdea && <RecipeCard recipeId={recipeIdea.recipeId} recipeName={recipeIdea.recipeName}/>}
+            {recipeIdea && <button type="button" onClick={addNewMealToDB}>Add Meal</button>}
         </div>
     );
 }
