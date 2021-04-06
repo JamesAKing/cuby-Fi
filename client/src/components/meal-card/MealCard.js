64fbe53822aed7b4e5f9cf61b1d096a178424cb7
James King <james_andrew_king@hotmail.co.uk>
2021-04-06 13:20:50 -0700

    STyled Meal Card to beter represent website desgin

--- client/src/components/meal-card/MealCard.js
+++ client/src/components/meal-card/MealCard.js
@@ -7,9 +7,9 @@ function MealCard({ day, recipeName, dayId, imgURL, toggleSelectMealModal, confi
             <div className="meal__overlay">
                 <h4 className="meal__text">{day}</h4>
                 <h3 className="meal__text">{recipeName}</h3>
-                <div id={dayId}>
-                    <button id={dayId} onClick={toggleSelectMealModal}>Select Meal</button>
-                    <button id={dayId} onClick={confirmMealEaten}>{recipeCooked ? "Recipe Eaten" : "Eat Recipe"}</button>
+                <div className="meal__btns" id={dayId}>
+                    <button className="btn" id={dayId} onClick={toggleSelectMealModal}>Select New Meal</button>
+                    <button className="btn" id={dayId} onClick={confirmMealEaten}>{recipeCooked ? "Recipe Eaten" : "Eat Recipe"}</button>
                 </div>
             </div>
         </li>
