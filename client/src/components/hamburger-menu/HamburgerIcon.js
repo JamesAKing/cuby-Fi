486f9527d2988aeebee1d15e92360caf7f2bfa00
James King <james_andrew_king@hotmail.co.uk>
2021-04-06 19:50:03 -0700

    created pull uot hamburger menu and added animations

--- client/src/components/hamburger-menu/HamburgerIcon.js
+++ client/src/components/hamburger-menu/HamburgerIcon.js
@@ -1,18 +1,32 @@
 import './HamburgerIcon.scss';
+import { useState } from "react";
 
-function BurgerMenuIcon(props) {
+function BurgerMenuIcon() {
+
+    const [ active, setActive ] = useState(false)
+
+    const rotateIcon = () => {
+        setActive(!active)
+    }
+
     return (
-        <div>
-            <div className="menu__icon">
-                <div className="menu__line"></div>
-                <div className="menu__line"></div>
-                <div className="menu__line"></div>
+        <>
+            <div className={!active ? "menu" : "menu menu__rotate"} onClick={rotateIcon}>
+                <div className="menu__icon">
+                    <div className="menu__line"></div>
+                    <div className="menu__line"></div>
+                    <div className="menu__line"></div>
+                </div>
+                <ul>
+                    {/* <li></li>  */}
+                </ul>
             </div>
-            <ul>
-                {/* <li></li>  */}
-            </ul>
-        </div>
+            <div className={!active ? "pull-out pull-out__inactive" : "pull-out pull-out__active"}>
+                
+            </div>
+        </>
     );
+
 }
 
 export default BurgerMenuIcon;
