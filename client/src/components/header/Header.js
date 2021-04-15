4f81ca67ff28d7ccfe842a7a35d2c768957748e4
James King <james_andrew_king@hotmail.co.uk>
2021-04-15 15:55:24 -0700

    bug fix: fixed pull out menu and header - prevented other componets overlaying them

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -6,23 +6,26 @@ import HamburgerIcon from '../hamburger-menu/HamburgerIcon';
 
 function Header() {
     return (
-        <header className="header">
-            <Link to="/">
-                <img className="header__logo" src={SquirrelLogo} alt="inFridge logo" />
-            </Link>
-            <Logo />
-            <nav>
-                <HamburgerIcon />
-                {/* <ul className="header__links">
-                    <li><Link className="header__link" to="/about">About</Link></li>
-                    <li><Link className="header__link" to="/contact-us">Contact Us</Link></li>
-                    <li><Link className="header__link" to="/cupboard">Cupboard</Link></li>
-                    <li><Link className="header__link" to="/recipe-book">Recipe Book</Link></li>
-                    <li><Link className="header__link" to="/schedule">Food Plan</Link></li>
-                    <li><Link className="header__link" to="/shopping-list">Shopping List</Link></li>
-                </ul> */}
-            </nav>
-        </header>
+        <>
+            <div className="header__spacer"></div>
+            <header className="header">
+                <Link to="/">
+                    <img className="header__logo" src={SquirrelLogo} alt="inFridge logo" />
+                </Link>
+                <Logo />
+                <nav>
+                    <HamburgerIcon />
+                    {/* <ul className="header__links">
+                        <li><Link className="header__link" to="/about">About</Link></li>
+                        <li><Link className="header__link" to="/contact-us">Contact Us</Link></li>
+                        <li><Link className="header__link" to="/cupboard">Cupboard</Link></li>
+                        <li><Link className="header__link" to="/recipe-book">Recipe Book</Link></li>
+                        <li><Link className="header__link" to="/schedule">Food Plan</Link></li>
+                        <li><Link className="header__link" to="/shopping-list">Shopping List</Link></li>
+                    </ul> */}
+                </nav>
+            </header>
+        </>
     );
 }
 
