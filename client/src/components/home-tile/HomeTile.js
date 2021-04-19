9fa3179f22181551f268c44f3149fedb96939d87
James King <james_andrew_king@hotmail.co.uk>
2021-04-19 11:48:58 -0700

    added correct colours to home tiles

--- client/src/components/home-tile/HomeTile.js
+++ client/src/components/home-tile/HomeTile.js
@@ -3,8 +3,29 @@ import { Link } from 'react-router-dom';
 
 function HomeTile({ title, linkUrl, linkImg, reversed }) {
 
+    let tile = "tile "
+
+    switch (title) {
+        case "Shopping List":
+            tile += "tile__color-shopping-list";
+            break;
+        case "Cupboard":
+            tile += "tile__color-cupboard";
+            break;
+        case "Weekly Meal Plan":
+            tile += "tile__color-meal-plan";
+            break;
+        case "Recipe Book":
+            tile += "tile__color-recipe-book";
+            break;
+        default: 
+            tile += null
+    }
+
+    console.log(tile)
+
     return (
-        <li className="tile">
+        <li className={tile}>
             <Link to={linkUrl} className={!reversed ? "tile__link" : "tile__link tile__link--reverse"}>
                 <div className="tile__content">
                     <h2 className="tile__text">{title}</h2>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ import RecipeBookIcon from "../assets/icons/recipe-book.png";
 import ShoppingListIcon from "../assets/icons/shopping-list.png";
 import HomeTile from '../components/home-tile/HomeTile';
 
-function Home(props) {
+function Home() {
 
     return (
         <nav className="home">
@@ -40,36 +40,6 @@ function Home(props) {
         </nav>
     )
 
-
-    // return (
-    //     <nav>
-    //         <ul className="home">
-    //             <li className="home__item home__item--one">
-    //                 <h2 className="home__title">Shopping List</h2>
-    //                 <Link to={shoppingList}><img className="home__link-icons" src={ShoppingListIcon} alt="Shopping List" /></Link>
-    //             </li>
-    //             <li className="home__item home__item--reverse home__item--two">
-    //                 <h2 className="home__title">Weekly Meal Plan</h2>
-    //                 <Link to={mealPlan}><img className="home__link-icons" src={CalendarIcon} alt="mealPlan" /></Link>
-    //             </li>
-    //             <li className="home__item home__item--three">
-    //                 <h2 className="home__title">Recipe Book</h2>
-    //                 <Link to={recipeBook}><img className="home__link-icons" src={RecipeBookIcon} alt="Recipe Book" /></Link>
-    //             </li>
-    //             {/* <li className="home__item home__item--reverse home__item--four">
-    //                 <h2 className="home__title">Cupboard</h2>
-    //                 <Link to={cupboard}><img className="home__link-icons" src={FoodItemsIcon} alt="Cupboard" /></Link>
-    //             </li> */}
-    //             <li className="home__item-container">
-    //                 {/* <h2 className="home__title">Cupboard</h2> */}
-    //                 <Link className="home__item home__item--reverse home__item--four" to={cupboard}>
-    //                     <h2 className="home__title">Cupboard</h2>
-    //                     <img className="home__link-icons" src={FoodItemsIcon} alt="Cupboard" />
-    //                 </Link>
-    //             </li>
-    //         </ul>
-    //     </nav>
-    // );
 }
 
 export default Home;
