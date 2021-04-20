949232a963556c4b3cda4a6d8813e6e5a2744881
James King <james_andrew_king@hotmail.co.uk>
2021-04-19 18:57:44 -0700

    added NavLink's to indicate to the user which section of the app they are in

--- client/src/components/hamburger-menu/PulloutMenu.js
+++ client/src/components/hamburger-menu/PulloutMenu.js
@@ -1,5 +1,5 @@
 import "./PulloutMenu.scss";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { home, cupboard, recipeBook, mealPlan, shoppingList } from '../../utilities/URLs';
 
 function PulloutMenu({ active, rotateIcon }) {
@@ -8,11 +8,16 @@ function PulloutMenu({ active, rotateIcon }) {
         <div className={!active ? "pull-out pull-out__inactive" : "pull-out pull-out__active"}>
             <nav className="pull-out__nav">
                 <ul className="pull-out__list">
-                    <li className="pull-out__item"><Link onClick={rotateIcon} to={home} className="pull-out__link">HOME</Link></li>
+                    {/* <li className="pull-out__item"><Link onClick={rotateIcon} to={home} className="pull-out__link">HOME</Link></li>
                     <li className="pull-out__item"><Link onClick={rotateIcon} to={shoppingList} className="pull-out__link pull-out__link--c">SHOPPING LIST</Link></li>
                     <li className="pull-out__item"><Link onClick={rotateIcon} to={mealPlan} className="pull-out__link pull-out__link--u">MEAL PLAN</Link></li>
                     <li className="pull-out__item"><Link onClick={rotateIcon} to={recipeBook} className="pull-out__link pull-out__link--b">RECIPE BOOK</Link></li>
-                    <li className="pull-out__item"><Link onClick={rotateIcon} to={cupboard} className="pull-out__link pull-out__link--y">CUPBOARD</Link></li>
+                    <li className="pull-out__item"><Link onClick={rotateIcon} to={cupboard} className="pull-out__link pull-out__link--y">CUPBOARD</Link></li> */}
+                    <li className="pull-out__item"><NavLink onClick={rotateIcon} to={home} className="pull-out__link" exact activeClassName="pull-out__link--selected">HOME</NavLink></li>
+                    <li className="pull-out__item"><NavLink onClick={rotateIcon} to={shoppingList} className="pull-out__link pull-out__link--c" activeClassName="pull-out__link--selected">SHOPPING LIST</NavLink></li>
+                    <li className="pull-out__item"><NavLink onClick={rotateIcon} to={mealPlan} className="pull-out__link pull-out__link--u" activeClassName="pull-out__link--selected">MEAL PLAN</NavLink></li>
+                    <li className="pull-out__item"><NavLink onClick={rotateIcon} to={recipeBook} className="pull-out__link pull-out__link--b" activeClassName="pull-out__link--selected">RECIPE BOOK</NavLink></li>
+                    <li className="pull-out__item"><NavLink onClick={rotateIcon} to={cupboard} className="pull-out__link pull-out__link--y" activeClassName="pull-out__link--selected">CUPBOARD</NavLink></li>
                 </ul>
             </nav>
         </div>
