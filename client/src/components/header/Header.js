c4d9b174596dd50db040bfb41226eb77e4a0f7a8
James King <james_andrew_king@hotmail.co.uk>
2021-04-19 18:44:51 -0700

    rebuilt header border to indicate to user which part of website tehy are in

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,14 +1,35 @@
 import './Header.scss';
 import SquirrelLogo from "../../assets/logos/squirrel-white.png";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Logo from '../global/Logo';
 import HamburgerIcon from '../hamburger-menu/HamburgerIcon';
 
 function Header() {
+
+    const currentUrl = useLocation().pathname.split('/')[1];
+    let headerCSS = "header ";
+
+    switch (currentUrl) {
+        case "shopping-list":
+            headerCSS += "header__color-secondary";
+            break;
+        case "meal-plan":
+            headerCSS += "header__color-three";
+            break;
+        case "recipe-book":
+            headerCSS += "header__color-four";
+            break;
+        case "cupboard":
+            headerCSS += "header__color-five";
+            break;
+        default:
+            break;
+    }   
+
     return (
         <>
             <div className="header__spacer"></div>
-            <header className="header">
+            <header className={headerCSS}>
                 <Link to="/">
                     <img className="header__logo" src={SquirrelLogo} alt="inFridge logo" />
                 </Link>
