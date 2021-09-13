3acc748c9272b7a56d0c88943e4dbafba32c91c3
James King <james_andrew_king@hotmail.co.uk>
2021-09-13 13:11:20 -0700

    added ScrollToTop component that returns user to top of page on page load

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { RecipesDB_URL } from './utilities/APIEndPoints';
 import { cupboard, recipeBook, mealPlan, shoppingList } from "./utilities/URLs";
+import ScrollToTop from './components/scroll-to-top/ScrollToTop';
 import Header from "./components/header/Header";
 import Home from './pages/Home';
 import ShoppingList from "./pages/ShoppingList";
@@ -29,6 +30,8 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+
+      <ScrollToTop />
       
       <Header />
 
